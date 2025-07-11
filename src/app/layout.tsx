23a7aacdbcaa316a6df4ae1e66142bcdb4e27717
shashwat2048 <shinghalshashwat@gmail.com>
2025-07-11 15:35:13 +0530

    Fix favicon path in layout component to ensure correct loading of the icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <link rel="shortcut icon" href="public/ecom.png" type="image/x-icon" />
+      <link rel="shortcut icon" href="/ecom.png" type="image/x-icon" />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
